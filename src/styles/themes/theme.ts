e4421d82954f3b5d18dbfc181e381cba334347a6
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-06 15:58:50 -0300

    feat: create themes and global styles

---
+++ src/styles/themes/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    background: '#feffff',
+    text: '#323232',
+    primary: '#1c3a89',
+    secondary: '#2c68e9',
+    yellow: '#fad000',
+  },
+  fonts: {
+    primary: 'Roboto',
+  },
+};
+
+export default theme;
