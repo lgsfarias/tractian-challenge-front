56fc646a926dfd75f1f43170dfdd131508f115bd
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-07 18:15:31 -0300

    refactor: refactor dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,29 @@
+import SideMenu from '../Menu';
+import React,{ useState} from 'react';
+import { Layout } from 'antd';
+import AppHeader from '../Header';
+
+const {Content, Sider} = Layout;
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Dashboard({children} : Props) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+        <div className="logo" />
+        <SideMenu />
+      </Sider>
+      <Layout className="site-layout">
+        <AppHeader/>
+        <Content style={{ margin: '0 16px' }}>
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -71,13 +71,4 @@ export const Global = createGlobalStyle`
     background-repeat: no-repeat;
     background-attachment: fixed;
   }
-  #components-layout-demo-side .logo {
-  height: 32px;
-  margin: 16px;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.site-layout .site-layout-background {
-  background: #fff;
-}
 `;
