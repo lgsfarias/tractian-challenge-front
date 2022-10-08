b18342075eb9608fa4f93c4e40af6f3b71b305fe
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-08 15:39:54 -0300

    feat: show user name and comany on dashboard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,34 @@
+import api from "../../services/api"
+import useAuth from "../../hooks/useAuth"
+import { useEffect, useState } from "react"
+import { User } from "../../interfaces"
+import { Typography } from "antd"
+
+const { Title } = Typography
+
 export default function Home() {
+  const { token } = useAuth()
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    (async () => {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await api.get('/users/show-data', config);
+      setUser(response.data);
+    })();
+  }, []);
+
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    user ? 
+    (<>
+      <Title level={2}>Welcome {user.name}</Title>
+      <h2>Company: {user.company.name}</h2>
+    </>) : <h1>Loading...</h1>
+
   )
 }
