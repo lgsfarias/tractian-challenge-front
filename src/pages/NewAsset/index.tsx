aa249c2909341e310bd059b85a8bb346e07b7f50
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-09 11:59:53 -0300

    feat: user can create assets

--- src/pages/NewAsset/index.tsx
+++ src/pages/NewAsset/index.tsx
@@ -2,24 +2,39 @@ import api from "../../services/api"
 import useAuth from "../../hooks/useAuth"
 import useCompany from "../../hooks/useCompany"
 import { useState } from "react"
-import { Button, Form, Input, Typography } from "antd"
+import { Button, Form, Input, Typography,Radio, Slider,Select,Space } from "antd"
 import useAlert from "../../hooks/useAlert"
 import { AxiosError } from "axios"
+import { Employee } from "../../interfaces"
 const { Title } = Typography
 
 export default function NewAsset() {
   const { setMessage } = useAlert();
   const { token ,companyId } = useAuth();
-  const { updateAssets } = useCompany();
+  const { updateAssets, units,employees } = useCompany();
   const [disabled, setDisabled] = useState(false);
+  const [unit, setUnit] = useState("");
+  const [form] = Form.useForm();
+
+  const status = [
+    'Running',
+    "Alerting",
+    'Stopped',
+  ]
+
+  const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 16 },
+  };
+
+  const tailLayout = {
+    wrapperCol: { offset: 6, span: 16 },
+  };
 
   const onFinish = async (values: any) => {
     setDisabled(true);
     try {
-      await api.post('/assets', {
-        ...values,
-        company:companyId,
-      }, 
+      await api.post('/assets', values, 
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -42,8 +57,14 @@ export default function NewAsset() {
     };
   };
 
+  const onReset = () => {
+    form.resetFields();
+  };
+
   return (
     <Form
+      {...layout}
+      form={form}
       name="add_asset"
       className="add_asset-form"
       initialValues={{ remember: true }}
@@ -52,12 +73,92 @@ export default function NewAsset() {
     >
       <Title level={1} className='title' >New Asset</Title>
       <Form.Item
+        name='image'
+        label='Image URL'
+        rules={[{required: true, message: 'Please input the image!' },{type:'url', message: 'Please input a valid URL!'}]}
+      >
+        <Input placeholder="Image"/>
+      </Form.Item>
+
+      <Form.Item
         name="name"
+        label="Name"
         rules={[{ required: true, message: 'Por favor insira o nome da unidade!' }]}
       >
         <Input placeholder="Asset's name"/>
       </Form.Item>
-      <Form.Item>
+      <Form.Item
+        name="model"
+        label="Model"
+        rules={[{ required: true, message: 'Por favor insira o modelo do ativo!' }]}
+      >
+        <Input placeholder="Asset's model"/>
+      </Form.Item>
+      <Form.Item
+        name="description"
+        label="Description"
+        rules={[{ required: true, message: 'Por favor insira a descrição da unidade!' }]}
+      >
+        <Input placeholder="Asset's description"/>
+      </Form.Item>
+      <Form.Item
+        name="status"
+        label="Status"
+        rules={[{ required: true, message: 'Por favor insira o status do ativo!' }]}
+      >
+        <Radio.Group
+        >
+          {status.map((item) => (
+            <Radio.Button value={item} key={item}>{item}</Radio.Button>
+          ))}
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item
+        name="healthLevel"
+        label="Health Level (%)"
+        rules={[{ required: true, message: 'Por favor insira o nível de saúde do ativo!' }]}
+      >
+        <Slider
+          marks={
+            {
+              0: '0%',
+              100: '100%',
+          }
+          }
+        />
+      </Form.Item>
+      <Form.Item
+        name="unit"
+        label="Unit"
+        rules={[{ required: true, message: 'Por favor insira a unidade do ativo!' }]}
+      >
+        <Select
+          placeholder="Select a unit"
+          allowClear
+          onChange={(value) => setUnit(value)}
+        >
+          {units.map((item) => (
+            <Select.Option value={item._id} key={item._id}>{item.name}</Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        name="owner"
+        label="Responsible"
+        rules={[{ required: true, message: 'Por favor insira o responsável pelo ativo!' }]}
+      >
+        <Select
+          placeholder={unit ? "Select a responsible" : "Select a unit first"}
+          allowClear
+        >
+          {employees.filter((employee: Employee) => employee.unit._id === unit).map((item) => (
+            <Select.Option value={item._id} key={item._id}>{item.name}</Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Space>
         <Button 
           type="primary" 
           htmlType="submit" 
@@ -66,6 +167,15 @@ export default function NewAsset() {
         >
           Add Asset
         </Button>
+        <Button
+          htmlType="button"
+          onClick={onReset}
+          className="login-form-button"
+          loading={disabled}
+        >
+          Reset
+        </Button>
+        </Space>
       </Form.Item>
     </Form>
   )
