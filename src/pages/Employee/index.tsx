c692efc91bbbac4c722dfb223c45da3511ac5737
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-11 17:46:12 -0300

    fix: employee page

--- src/pages/Employee/index.tsx
+++ src/pages/Employee/index.tsx
@@ -2,5 +2,10 @@ import { useParams } from 'react-router-dom';
 
 export default function Employee() {
   const { id } = useParams();
-  return <h1>Employee {id}</h1>;
+  return (
+    <>
+      <h1>Employee {id} </h1>;
+      <h2> This Page will be implemented soon </h2>
+    </>
+  );
 }
